522e085ff68573f3026bf549acbe911d9e0234c8
steve wambua <stevemighty2030@gmail.com>
2023-03-25 12:51:21 +0300

    initial commit

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+    public static void main(String[] args){
+        for (int i=0; i<=10; i++){
+            System.out.println("hello mr steve..."+i);
+        }
+        int age=56;
+        System.out.println("my age is:" +age);
+
+    }
+}
